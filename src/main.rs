06cadd8de0519142d2195e2b5b41fafd6efba7c6
Imran-S-heikh <imransheikhsadi@gmail.com>
2022-02-25 12:17:27 +0600

    Server Updated from Single Thread to Multi Thread

--- src/main.rs
+++ src/main.rs
@@ -2,26 +2,47 @@ use std::{
     fs,
     io::{Read, Write},
     net::{TcpListener, TcpStream},
+    thread,
+    time::Duration,
 };
+
+use webserver_rust::ThreadPool;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        handle_connection(stream);
+        pool.execute(|| {
+            handle_connection(stream);
+        });
+
+
     }
 }
 
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
-
     stream.read(&mut buffer).unwrap();
 
-    let content = fs::read_to_string("./public/index.html").unwrap();
+    let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
+
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK", "./public/index.html")
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(5));
+        ("HTTP/1.1 200 OK", "./public/index.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "./public/404.html")
+    };
+
+    let content = fs::read_to_string(filename).unwrap();
     let response = format!(
-        "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{content}",
+        "{}\r\nContent-Length: {}\r\n\r\n{content}",
+        status_line,
         content.len()
     );
 
